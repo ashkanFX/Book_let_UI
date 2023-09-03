94af6937d51b44a69e80717f09d11737b18b0bdc
ashkan <askanfotohi81@gmail.com>
2023-09-03 17:54:36 +0330

    // fix menu button  1206

--- src/app/structure/project/view-page/view-page-routing.module.ts
+++ src/app/structure/project/view-page/view-page-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-/*  {path : '*!/'  ,redirectTo : 'view'},*/
+  {path : ''  ,redirectTo : '/view' , pathMatch : 'full'},
   {path : 'view' , component:HomeComponent}
 ];
 
