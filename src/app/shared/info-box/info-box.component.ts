53724eb2fadf215cd8fb312c362e11851a8ec439
ashkan <askanfotohi81@gmail.com>
2023-11-03 11:51:02 +0330

    1207

--- src/app/shared/info-box/info-box.component.ts
+++ src/app/shared/info-box/info-box.component.ts
@@ -1,12 +1,35 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input} from '@angular/core';
 import {InfoBoxEnum} from "../../enums/infoBox.enum";
 
 @Component({
+  // [textStyle][header] for make these two required to pass
   selector: 'info-box[textStyle][header]',
   templateUrl: './info-box.component.html',
   styleUrls: ['./info-box.component.css']
 })
-export class InfoBoxComponent {
-  @Input() textStyle: InfoBoxEnum = InfoBoxEnum.center
-  @Input('header') header: string | null
+export class InfoBoxComponent implements AfterViewInit ,DoCheck {
+  @Input() textStyle: InfoBoxEnum = InfoBoxEnum.center;
+  @Input('header') header: string | null;
+  @ContentChild('text') text: ElementRef;
+  // private _message: string = '';
+  //
+  // @Input()
+  // set message(value: string) {
+  //
+  // }
+  // get reversedMessage(): string {
+  //
+  // }
+
+  ngAfterViewInit(): void {
+    console.log(this.text.nativeElement.innerHTML.length)
+    if (this.text.nativeElement.innerHTML.length >= 500) {
+      this.text.nativeElement.innerHTML = this.text.nativeElement.innerHTML.substring(0, 450)
+    }
+  }
+
+  ngDoCheck(): void {
+    console.log(this._message)
+  }
+
 }
--- src/app/structure/view-page/home/home.component.ts
+++ src/app/structure/view-page/home/home.component.ts
@@ -6,6 +6,7 @@ import {InfoBoxEnum} from "../../../enums/infoBox.enum";
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+
 })
 export class HomeComponent implements OnInit {
   classScroll = "hidden"
